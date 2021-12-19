d4e91880b309f7cef508e78c73c5443e51c4559b
Julia Song <julja.song@gmail.com>
2021-12-19 20:53:42 +0900

    필드 동기화-개발

---
+++ src/test/java/hello/advanced/trace/logTrace/FieldLogTraceTest.java
@@ -0,0 +1,38 @@
+package hello.advanced.trace.logTrace;
+
+import hello.advanced.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FieldLogTraceTest {
+
+    FieldLogTrace trace = new FieldLogTrace();
+
+    @Test
+    public void begin_end_level2() throws Exception {
+        //given
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.begin("hello2");
+
+        //when
+        
+        //then
+        trace.end(status2);
+        trace.end(status1);
+    }
+
+    @Test
+    public void begin_exception_level2() throws Exception {
+        //given
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.begin("hello2");
+
+        //when
+
+        //then
+        trace.exception(status2, new IllegalStateException());
+        trace.exception(status1, new IllegalStateException());
+    }
+
+}
